e3c29ddba20c0adc7d1ee2c019c7b767a8fcf492
Rajesh-git-web <79069346+Rajesh-git-web@users.noreply.github.com>
2021-11-02 13:16:15 +0530

    Create kumar.sql

---
+++ kumar.sql
@@ -0,0 +1,19 @@
+CREATE DATABASE 123_xyz
+GO
+
+USE 123_xyz
+GO
+
+CREATE TABLE dbo.Employee (
+	Emp_ID int IDENTITY(1, 1),
+	Emp_Name varchar(100),
+	Emp_Salary decimal(10, 2)
+)
+
+CREATE TABLE dbo.Employee_Audit2 (
+	Emp_ID int,
+	Emp_Name varchar(100),
+	Emp_Salary decimal(10, 2),
+	Audit_Action varchar(100),
+	Audit_Timestamp datetime
+)
