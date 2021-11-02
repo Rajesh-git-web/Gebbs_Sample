d41e20d67013026491f6bc1e8357a1fddf35f967
Rajesh-git-web <79069346+Rajesh-git-web@users.noreply.github.com>
2021-11-02 13:17:54 +0530

    Update rajesh.sql

--- rajesh.sql
+++ rajesh.sql
@@ -19,9 +19,4 @@ CREATE TABLE dbo.Employee_Audit2 (
 	Audit_Action varchar(100),
 	Audit_Timestamp datetime
 )
-
-INSERT INTO dbo.Employee VALUES ('Anees', 1000)
-INSERT INTO dbo.Employee VALUES ('Rick', 1200)
-INSERT INTO dbo.Employee VALUES ('John', 1100)
-INSERT INTO dbo.Employee VALUES ('Stephan', 1300)
-INSERT INTO dbo.Employee VALUES ('Maria', 1400)
+GO
