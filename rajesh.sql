102d7861419d6b4e4f6072d0ab6f71dc350756aa
Rajesh-git-web <79069346+Rajesh-git-web@users.noreply.github.com>
2021-11-10 02:06:09 +0530

    Update rajesh.sql

--- rajesh.sql
+++ rajesh.sql
@@ -15,7 +15,7 @@ CREATE TABLE dbo.Employee (
 CREATE TABLE dbo.Employee_Audit2 (
 	Emp_ID int,
 	Emp_Name varchar(110),
-	Emp_Salary decimal(10, 2),
+	Emp_Salary decimal(11, 1),
 	Audit_Action varchar(110),
 	Audit_Timestamp datetime
 )
